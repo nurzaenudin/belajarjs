1106d002745b20939d95d4517474b15bb732c281
udin <nurzaenudin@gmail.com>
2017-10-04 03:01:59 -0700

    seeder

--- database/seeds/ItemsSeeder.php
+++ database/seeds/ItemsSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Item;
 
-class DatabaseSeeder extends Seeder
+class ItemsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,7 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-		$this->call(ItemsSeeder::class);
+	
+		$item= new Item();
+		$item->name='Kertas';
+		$item->price=1000;
+		$item->save();
+		//
     }
 }
